8c43890f92b16bbe10d1e1734a8ca9537069a957
Edward Phillips <edd0ddi0@gmail.com>
2020-04-05 15:01:35 +0100

    test refactor

--- spec/link_sort_spec.rb
+++ spec/link_sort_spec.rb
@@ -3,11 +3,7 @@ require 'link_sort'
 describe LinkSort do
   context "finding the order" do
     it "should be able to open the challenge map file" do
-      expect(LinkSort.find_order). to eq(" LINK ORDER:
- 1. mefirst.md
- 2. mesecond.md
- 3. ozymandias.md
- 4. shhhnonames.md")
+      expect { LinkSort.find_order }.to_not raise_error
     end
   end
 end
